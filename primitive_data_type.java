e0e9a84fbe0e23c39269209672c8ca65788c055f
Pubbagiri Bhanu Reddy <bhanureddypubbagiri@gmail.com>
2026-01-11 23:47:01 +0530

    Add files via upload

---
+++ primitive_data_type.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class primitive_data_type
+{
+static byte b;
+static short s;
+static int i;
+static long l;
+static float f;
+static double d;
+static char c;
+static Boolean B;
+public static void main(String args[])
+{
+System.out.println("byte value:"+b);
+System.out.println("short value:"+s);
+System.out.println("int value:"+i);
+System.out.println("long value:"+l);
+System.out.println("double value:"+d);
+System.out.println("float value:"+f);
+System.out.println("character value:"+c);
+System.out.println("boolean value:"+B);
+}
+}
