41d1d7489e1c1f228e605b1bdfea7a8b13c875ae
Pubbagiri Bhanu Reddy <bhanureddypubbagiri@gmail.com>
2026-01-11 23:46:11 +0530

    Add files via upload

---
+++ multiple_catches.java
@@ -0,0 +1,24 @@
+class multiple_catches
+{
+    public static void main(String args[])
+    {
+try
+{
+   int a[]={1,2,3,4};
+   System.out.println(a[10]);
+}
+catch(Exception e1)
+{
+   System.out.println("ArrayIndexOutOfBoundsException occurred="+e1.getMessage());
+}
+try{
+    int data=100/0; //may throw exception
+    System.out.println(data);
+}
+
+catch(ArithmeticException e2)
+{
+    System.out.println("ArithmeticException occurred="+e2.getMessage());
+}
+    }
+}
